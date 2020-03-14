0422da938d80570d2fd235ff332343c3882f9ca7
gonza <gonzalo.christobal@gmail.com>
2020-03-14 17:48:22 -0300

    Initial Commit

---
+++ MenuBarTimer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MenuBarTimer
+//
+//  Created by Gonzalo Rodríguez on 3/14/20.
+//  Copyright © 2020 Gonzalo Rodríguez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MenuBarTimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MenuBarTimer
+//
+//  Created by Gonzalo Rodríguez on 3/14/20.
+//  Copyright © 2020 Gonzalo Rodríguez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
