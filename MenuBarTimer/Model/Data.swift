3ef94e96f8cf7924d34bd11024c2d5c33ceb3094
gonza <gonzalo.christobal@gmail.com>
2020-03-18 22:16:10 -0300

    Add icons

---
+++ MenuBarTimer/Model/Data.swift
@@ -0,0 +1,14 @@
+//
+//  CSV.swift
+//  MenuBarTimer
+//
+//  Created by Gonzalo Rodríguez on 3/18/20.
+//  Copyright © 2020 Gonzalo Rodríguez. All rights reserved.
+//
+
+import Foundation
+import CSV
+
+class Data {
+    
+}
---
+++ MenuBarTimer/Model/Helper.swift
@@ -0,0 +1,9 @@
+//
+//  Helper.swift
+//  MenuBarTimer
+//
+//  Created by Gonzalo Rodríguez on 3/18/20.
+//  Copyright © 2020 Gonzalo Rodríguez. All rights reserved.
+//
+
+import Foundation
